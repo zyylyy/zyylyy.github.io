0f4fcbfbff30567edb45461bc9b2f94f4c6c7f01
zhouyue <zhouyue@126.com>
2017-06-27 15:09:10 +0800

    删除替换

--- App_SalesTollModules/common.7debbfbfc802b71e8592.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],[function(n,o,p){p(56),p(58),p(68),n.exports=p(182)}]);
-//# sourceMappingURL=common.7debbfbfc802b71e8592.js.map
--- src/services/fetch/config.js
+++
@@ -1,8 +0,0 @@
-// 此处配置根路径以及全局错误处理
-export const rootPath = 'http://192.168.88.149:88/api'
-// export const rootPath = 'http://vapi.loonxierp.com/api'
-// export const rootPath = 'http://suz-api.loonxierp.com/api'
-
-export const errHandler = (e) => {
-	console.log(e)
-}
